2dd798d0cd698d3ac044074af5a2ea03f1c71bce
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-02-12 01:19:20 -0700

    KD 4

--- final.py
+++ final.py
@@ -5,39 +5,58 @@ import random
 import time
 import os
 
+# Classes for all displayable tiles (including player and enemies) --------------------------------------------------------
+
 class Player:
-    def __init__(self):
+    def __init__(self, char):
         self.health = 100
         self.room = 0
-        self.coord = (3,3) #MAKE THIS CORRECT LATER
+        self.char = char
+        self.coord = (5,7)
         self.inv = ["Sword"]
         self.weapon = "Sword"
         
-    def interact(self):
-        #Look for surrounding chests/doors
-        #if a chest is found call choose_item() on it and add the result to the inventory
-        pass
+    def interact(self, map):
+        for tile in map.values():
+            if type(tile) == Chest and tile.coord in area((),()):
+                pass
 
-    def use(self):
-        if keyboard.is_pressed("e"):
-            os.system("cls")
-            
-            print("Select an item to use:")
+            elif type(tile) == Door and tile.coord in area((),()):
+                pass
+
+        #if a chest is found call choose_item() on it and add the result to the inventory
+
+    def inventory(self):
+        os.system("cls")
+        
+        while True:
+            print("Select an item to use (or type 'Exit' to cancel):")
             for item in self.inv:
                 print(f"- {item}")
             
-            item = input()
+            item = input().strip().capitalize()
             if item in self.inv:
-                if item == "Potion":
+                if item == "Health Potion":
                     self.health += 50
                     if self.health > 100: self.health == 100
-                elif item == "Sword":
-                    self.weapon =
+                    self.inv.remove("Health Potion")
+                elif item == "Sword": self.weapon = "Sword"
+                elif item == "Spear": self.weapon = "Spear"
+                elif item == "Bomb": self.weapon = "Bomb"
                 
+                break
+            elif item == "Exit": break
+            print()
+
+    def useWeapon(self):
+        pass
 
 class Enemy:
     def __init__(self, kind, coord):
-        self.kind = kind
+        self.char = kind
+        if kind == "*": self.health = 10
+        elif kind == "0": self.health = 100
+        else: self.health = 1000
         self.coord = coord
     
     def behavior():
@@ -45,29 +64,40 @@ class Enemy:
 
 class Wall:
     def __init__(self, coord):
+        self.char = "█"
         self.coord = coord
 
 class Door:
-    def __init__(self, coord, orientation, new_room):
+    def __init__(self, coord, orientation, new_room, new_coord):
         self.coord = coord
-        self.orientation = orientation
+        self.char = orientation
         self.new_room = new_room
+        self.new_coord = new_coord
 
 class Chest:
-    def __init__(self, contents, coord):
-        self.contents = contents
+    def __init__(self, coord):
+        self.char = "☐"
         self.coord = coord
-        self.table = [("bomb", 0.1), ("spear", 0.1), ("potion", 0.4)]
+        self.table = [("Bomb", 0.1), ("Spear", 0.1), ("Health Potion", 0.4)]
         
-    def choose_item(self):
+    def chooseItem(self):
         while True:
             item = random.choice(self.table)
             if random.random() < item[1]:
                 return item[0]
 
+# Helper functions / variables --------------------------------------------------------
+
 game = [
-    {},
-    {},
+    {(x, y): Wall((x, y)) for x, y in zip(
+        [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 2, 17, 1, 18, 1, 1, 1, 18, 1, 18, 1, 18, 1, 1, 1, 18, 2, 17, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8, 8, 9, 10, 11, 11, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13]
+    )} | {(18, 4): Door((18, 4), "|", 1, (2,7)), (18, 5): Door((18, 5), "|", 1, (2,8)), (18, 9): Door((18, 9), "|", 2, (2,2)), (18, 10): Door((18, 10), "|", 1, (2,3))},
+    
+    {(x, y): Wall((x, y)) for x, y in zip(
+        [12, 13, 14, 15, 16, 17, 18, 19, 12, 19, 12, 19, 12, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 19, 19, 19, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+        )} | {},
     {},
     {},
     {},
@@ -80,16 +110,53 @@ game = [
     {}
 ]
 
-def area(x1, y1, x2, y2):
+def area(coord1, coord2):
     tiles = []
-    for i in range(x1, x2+1):
-        for j in range(y2, y1, -1):
-            tiles.append((i,j))
+    x1, y1 = coord1
+    x2, y2 = coord2
+    x_start, x_end = min(x1, x2), max(x1, x2)
+    y_start, y_end = min(y1, y2), max(y1, y2)
+    for y in range(y_start, y_end + 1):
+        for x in range(x_start, x_end + 1):
+            tiles.append((x, y))
     return tiles
 
-def display():
+def move(player):
+    dx = 0
+    dy = 0
+
+    if keyboard.is_pressed("up") or keyboard.is_pressed("w"): dy = -1
+    if keyboard.is_pressed("down") or keyboard.is_pressed("s"): dy = 1
+    if keyboard.is_pressed("left") or keyboard.is_pressed("a"): dx = -1
+    if keyboard.is_pressed("right") or keyboard.is_pressed("d"): dx = 1
+
+    new_coord = (player.coord[0] + dx, player.coord[1] + dy)
+    room = game[player.room]
+    tile = room.get(new_coord)
+
+    if isinstance(tile, Door):
+        player.room = tile.new_room
+        player.coord = tile.new_coord
+        return
+
+    for coord in area(player.coord, new_coord): 
+        if coord in room: return
+
+    player.coord = new_coord
+
+def display(player):
     os.system("cls")
-    
+
+    prevY = 1
+    for coord in area((0,0), (max([k[0] for k in game[player.room].keys()])+1, max([k[1] for k in game[player.room].keys()])+1)):
+        if prevY != coord[1]: print()
+        try: print(game[player.room][coord].char, end="")
+        except: 
+            if coord == player.coord: print(player.char, end="")
+            else: print(" ", end="")
+        prevY=coord[1]
+
+# Main loop --------------------------------------------------------
 
 def main():
     while True:
@@ -103,26 +170,25 @@ def main():
         else:
             break
 
-
     _ = input(f"""You are {char}.
 LORELORELORE
 
 Use the arrow keys (or WASD) to move
 Use E to open the inventory
 Use LMB to attack with your chosen weapon
-Use SPACE to open chests(☐) or doors (|,-)(You have to be next to them to open them)
+Use SPACE to open chests(☐)
 
 Press ENTER to start your adventure!
 """)
 
+    player = Player(char)
         
     while True:
-
+        if keyboard.is_pressed("e"): player.use()
         if keyboard.is_pressed("esc"): break
-        time.sleep(0.1)
+
+        move(player)
+        display(player)
+        time.sleep(.075)
 
 main()
-
-def invent():
-    if keyboard.is_pressed("E"):
-        pass
